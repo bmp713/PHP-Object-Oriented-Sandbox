7657563c1001974519e250baae7b9e35ca4a6d4a
Brandon Michael Piper <bmp713@yahoo.com>
2018-06-20 10:52:17 -0700

    Add files via upload

--- index.php
+++ index.php
@@ -4,12 +4,30 @@
 <title>PHP Object Oriented | Sandbox</title>
 </head>
 <?php
-/* Abstract class cannot be instantiated.
-   Other classes can extend an abstract class to gain 
-   access to protected and public members, but not private.*/ 
+
+
+echo '//Ternary Operators<br>';
+// echo ( condition ) ? if TRUE execute : if FALSE execute;
+$name_1 = 'Michael';
+$name_2 = 'Steve';
+echo ( $name_1 == $name_2 ) ? 'TRUE' : 'FALSE' ; 
+echo '<br>';
+echo ( $name_1 != $name_2 ) ? 'TRUE' : 'FALSE' ; 
+echo '<br><br>';
+
+$loggedIn = false;
+$isTrue = ($loggedIn == true) ? true: false;
+echo '$isTrue = '.$isTrue.'<br>';
+echo '<br>';
+
+
+echo 
+	'//Abstract class cannot be instantiated.<br>
+    Other classes can extend an abstract class to gain<br>
+    access to protected and public members, but not private.'; 
 abstract class Customer{
-	private $id;
-	private $name;
+	protected $id;
+	protected $name;
 	// Only accessed from self and child extending class
 	protected $password; 
 
@@ -29,7 +47,7 @@ class Account extends Customer{
 	public function __construct($id, $name, $password, $balance){
 		parent::__construct($id, $name, $password);
 		$this->balance = $balance;
-		echo __CLASS__.'<br>';
+		echo '<br>'.__CLASS__.'<br>';
 		echo __LINE__.'<br>';
 		echo __DIR__.'<br>';		
 		echo __METHOD__.'<br>';
@@ -40,9 +58,10 @@ class Account extends Customer{
 		return $this->balance;
 	}
 	public function printAccount(){
-		echo $this->id.', ';
-		echo $this->name.', ';
-		echo $this->getPassword();
+		echo $this->id.', '.
+		$this->name.', '.
+		$this->getPassword().', '.
+		$this->balance;
 	}
 	public function getPassword(){
 		return $this->password;
@@ -51,10 +70,11 @@ class Account extends Customer{
 $customer = new Account(1, 'Steve Jobs', 'Password123', 99);
 echo 'Balance = '.$customer->getBalance().'<br>';
 echo 'Account = ';
-$customer->printAccount().'<br>';
-echo 'Password = '.$customer->getPassword().'<br><br>';
+$customer->printAccount();
+echo '<br>Password = '.$customer->getPassword().'<br><br>';
 
-/* Accessing static variable/function without instantiation */
+
+echo '//Accessing static variable/function without instantiation<br>';
 class Name{
 	public static $name = 'Name';
 	private $first;
@@ -82,8 +102,26 @@ echo $name->getFirst().'<br>';
 echo $name->getLast().'<br><br>'; // prints “MyName”
 
 
-/* Sorting Algorithms
-numbers = [ 4 5 7 8 4 3 7  6 5 4 6 ] */
+echo '//Arrays<br>';
+$cars = array('Honda','Toyota','Ferrari','BMW');
+foreach( $cars as $make ){
+	echo $make.'<br>';
+}
+echo '<br>';
+
+
+echo '//Associative Array<br>';
+$people = array('Michael' => 'Ferrari', 'Steve' => 'Mercedes',
+				'Vanessa' => 'Toyota', 'Kate' => 'Honda');
+
+foreach( $people as $person => $car ){
+	echo $person.', ';
+	echo $car.'<br>';
+}
+echo '<br>';
+
+
+echo '//Sorting Algorithms and Recursion<br>';
 $numbers = array("5", "2", "3", "7", "4", "0", "9");
 print_r( $numbers ); 
 echo '<br>';
@@ -120,9 +158,8 @@ function recurse( $number ){
 $result = recurse( 22 );
 echo '<br>';
 
+
 ?>
 </body>
 </html>
-
-
 
